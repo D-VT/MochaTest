db8f7cca47b168712d8343e03b86e2967e8d0a58
D-VT <812303220@qq.com>
2019-04-29 00:18:55 +0800

    Update testJS.js

--- testJS.js
+++ testJS.js
@@ -1,6 +1,5 @@
 var request = require('supertest');
 var expect = require('chai').expect;
-var should = require('should');
 var fs = require('fs');
 var url = "http://preview.airwallex.com:30001";
 var dataFileName = 'testCaseJson.json';
@@ -16,7 +15,6 @@ describe('测试状态码',function(){
 			.send(test.info)
 			.end(function (error,response){
 			expect(response.statusCode).to.equal(test.response.statusCode);
-//			should(response.statusCode).eqls(test.response.statusCode);
 			if(error) throw error;
 			done();
 		})
